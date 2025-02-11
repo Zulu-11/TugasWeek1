6c294e93092a4544ffee40c78c597cabcd1fdd4a
Zulu-11 <95024585+Zulu-11@users.noreply.github.com>
2025-02-11 16:57:00 +0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Raka Satya Wurya Andhika – 00000070105</Text>
+    </View>
+  );
+}
